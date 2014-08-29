47104dac113ddcb4c85182ac67012f8ea2b9aeba
mbelousov <belousov.maks@gmail.com>
2014-08-29 01:30:39 +0100

    Code improved and refactored, README provided

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-heroku-deploy-go
-================
+# Go app deployment tool for Heroku
+Bash script for [Go](http://golang.org/) app deployment to [Heroku](http://heroku.com/)
+===================================
+## Usage
+This tool should be called from app directory.
+(i.e. `GOPATH/src/github.com/mbelousov/demoapp`)
 
-Bash script to deploy Go apps to heroku
+Simply call 
+`deploy.sh app_name [procfile] [config] [buildpack]` from your package directory.
+### Arguments
+  1. `app_name`  - heroku application name
+  2. `procfile`  - content of Procfile (optional, use "" to skip)
+  3. `config`    - heroku settings (optional, use "" to skip)
+  4. `buildpack` - Heroku buildpack (optional, use [heroku-buildpack-go](https://github.com/kr/heroku-buildpack-go) by default)
+
+Order is important.
+===================================
+## Example:
+`deploy.sh demoapp "web: demoapp" "APP_SETTING=VALUE" "https://github.com/mbelousov/heroku-buildpack-go-revel"`
+
+or call `deploy.sh` (without arguments) for help.
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+APP_NAME=$1
+PROCFILE=$2
+CONFIG=$3
+BUILDPACK=$4
+DEFAULT_BUILDPACK="https://github.com/kr/heroku-buildpack-go"
 
-function output {
-	echo "----> "$1
-}
 function pathcombine {
 	if [[ "$1" == *\\ ]]
 	then
@@ -12,42 +14,53 @@ function pathcombine {
 	fi	
 }
 
-REPO=$1
-REPO_MODE=$2
-APP_NAME=$3
-PROCFILE=$4
-CONFIG=$5
-IGNORE=$6
-BUILDPACK=$7
-DEFAULT_BUILDPACK="https://github.com/kr/heroku-buildpack-go"
-DEFAULT_REPO_MODE='remote'
-echo ""
+function output {
+	echo "----> "$1
+}
+function output_usage {
+	echo '----------------------------------------'
+	echo 'Heroku deployment tool'
+	echo ' '
+	echo 'This tool should be called from app directory.'
+	echo '(i.e. GOPATH/src/github.com/mbelousov/demoapp)'
+	echo '----------------------------------------'
+	echo 'Usage: '
+	echo 'deploy.sh app_name [procfile] [config] [buildpack]'
+	echo '  app_name  - heroku application name'
+	echo '  procfile  - content of Procfile (optional, use "" to skip)'
+	echo '  config - heroku settings (optional, use "" to skip)'
+	echo '  buildpack - Heroku buildpack (optional)'
+	echo '                (default: '$DEFAULT_BUILDPACK')'
+	echo '----------------------------------------'
+	echo 'Example:'
+	echo 'deploy.sh demoapp "web: demoapp" "APP_SETTING=VALUE" "https://github.com/mbelousov/heroku-buildpack-go-revel"'
+}
+if [[ $APP_NAME == "help" ]]; then
+	output_usage
+	exit
+fi
 
-if test -z "$REPO_MODE"; then
-	output 'Mode is not provided. Using default..'
-	REPO_MODE=$DEFAULT_REPO_MODE
+CWD=$(pwd)
+GOSRC=$(cd $GOPATH/src/ && pwd)
+if [[ ! $CWD == $GOSRC* ]]; then
+	output 'Repository package is not in GOPATH!'
+	output_usage
+	exit
 fi
-if [[ $REPO_MODE != "remote" ]] && [[ $REPO_MODE != "local" ]] ; then
-	output "Invalid repo mode. Using default.."
-	REPO_MODE=$DEFAULT_REPO_MODE
-fi
-output 'Mode: '$REPO_MODE
-if [[ $REPO_MODE == "local" ]]; then
-	if test -z "$GOPATH"; then
-		output "GOPATH is empty"
-		exit
-	fi
-	REPO_URL=$(pathcombine $GOPATH src/$REPO)
-else
-	REPO_URL="https://"$REPO
-fi
-output 'Repository: '$REPO_URL
+
+# getting full package name
+
+REPO=${CWD:${#GOSRC}+1:${#CWD}-${#GOSRC}-1}
+
+
 if test -z "$REPO"; then
 	output "Repository is not provided"
+	output_usage
 	exit
 fi
 if test -z "$APP_NAME"; then
 	output "Heroku App name is empty"
+	output_usage
 	exit
 fi
 
@@ -58,67 +71,44 @@ if test -z "$BUILDPACK"; then
 fi
 output "Buildpack: "$BUILDPACK
 
-CURRENT=$(date +%s)
 REPO_NAME=$(echo ${REPO_URL##*/} | awk -F '.' '{ print $1 }')
-DEPLOY_DIR='.deploy'
-REPO_PATH=$DEPLOY_DIR"/"$REPO_NAME
 
-LOCAL_REPO=$(pathcombine $GOPATH src/$REPO)
-if [ ! -d "$DEPLOY_DIR" ]; then
-	mkdir "$DEPLOY_DIR"
-fi
 
-output 'Removing '$REPO_PATH'..'
-rm -rf $REPO_PATH
 
-if [ ! -d "$REPO_PATH" ]; then
-	output 'Cloning into '$REPO_PATH'..'
-	git clone $REPO_URL $REPO_PATH
-	if [[ ! $? = 0 ]]; then
-		exit
-	fi
-fi	
-
-output 'Change dir to '$REPO_PATH
-cd $REPO_PATH
-output 'Fetching..'
-git fetch origin
-output 'Reseting to origin..'
-git reset --hard origin/master && git clean -fd
-
-output 'Pulling from origin..'
-git pull --force origin master
+output 'Checking for modifications..'
+git init 
 if [[ ! $? = 0 ]]; then
 	exit
 fi
 
+if ! test -z "$(git status --porcelain)"; then
+    output 'Repository is dirty. Please commit or discard your changes.'
+    exit
+fi
+
+
+
 output 'Heroku initialization..'
 heroku login
-output 'Checking heroku remote..'
-git remote show heroku > /dev/null 2>&1 #git ls-remote --exit-code heroku
+output 'Adding heroku remote..'
+git remote rm heroku > /dev/null 2>&1
+heroku git:remote -a $APP_NAME
 if [[ ! $? = 0 ]]; then
-	output 'Adding heroku remote..'
-	heroku git:remote -a $APP_NAME
-	if [[ ! $? = 0 ]]; then
-		exit
-	fi
+	exit
 fi
+
 output 'Setting configurations..'
-heroku config:set BUILDPACK_URL=$BUILDPACK $CONFIG
+heroku config:set BUILDPACK_URL=$BUILDPACK $CONFIG > /dev/null
+if [[ ! $? = 0 ]]; then
+	exit
+fi
 
 output 'Creating Procfile..'
 echo $PROCFILE > Procfile
 output 'Getting dependencies..'
-godep save
-if [[ ! $? = 0 ]]; then
-	echo $REPO > .godir
-fi
+godep save ./...
 
-if ! test -z "$IGNORE"; then
-	output 'Updating index..'
-	printf "\n"$IGNORE"\n" >> .gitignore
-	git rm -r --cached .
-fi
+echo $REPO > .godir
 
 git add -A .
 git commit -m "heroku procfile and dependencies"
@@ -129,14 +119,19 @@ if [[ ! $? = 0 ]]; then
 	exit
 fi
 
-#output 'Scaling..'
-#heroku ps:scale web=1
+
 heroku ps
 
 heroku open
 
 
-#cd '../../'
-#output 'Removing '$REPO_PATH'..'
-#rm -rf $REPO_PATH
 
+output 'Removing Godeps..'
+rm -rf $CWD/Godeps
+output 'Removing .godir..'
+rm -f $CWD/.godir
+output 'Removing .Procfile..'
+rm -f $CWD/Procfile
+
+output 'Reseting to previous commit..'
+git reset --hard HEAD~1 && git clean -fd
